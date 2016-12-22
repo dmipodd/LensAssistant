114471fa0e9990e75846a2a25bcc950bc992ab46
dmipodd <dmipodd@gmail.com>
2016-12-22 23:23:48 +0300

    "Add new" form validation

--- src/main/resources/public/controllers/addNewLensesController.js
+++ src/main/resources/public/controllers/addNewLensesController.js
@@ -1,16 +1,23 @@
 angular.module("lensAssistant")
-.controller("addNewLensesController", function ($scope, lensRepositoryService, $location) {
-	$scope.rightLensPanelVisible = false;
-	$scope.showRightLensPanel = function (sameAsLeftLens) {
-		if (sameAsLeftLens) {
-			$scope.rightLens = angular.copy($scope.leftLens);
-		} else {
-			$scope.rightLens = {};
-		}
-		$scope.rightLensPanelVisible = true;
-	}
-	$scope.saveNew = function () {
-		lensRepositoryService.save($scope.rightLens, $scope.leftLens);
-		$location.path("/summary");
-	}
-});
+    .controller("addNewLensesController", function ($scope, lensRepositoryService, $location) {
+        $scope.rightLensPanelVisible = false;
+        $scope.showRightLensPanel = function (sameAsLeftLens) {
+            if (sameAsLeftLens) {
+                $scope.rightLens = angular.copy($scope.leftLens);
+            } else {
+                $scope.rightLens = {};
+            }
+            $scope.rightLensPanelVisible = true;
+        }
+
+        $scope.lensesFormsAreValid = function () {
+            return $scope.leftLensFormIsValid && (!$scope.rightLensPanelVisible || $scope.rightLensFormIsValid);
+        }
+
+        $scope.saveNew = function () {
+            if ($scope.lensesFormsAreValid()) {
+                lensRepositoryService.save($scope.rightLens, $scope.leftLens);
+                $location.path("/summary");
+            }
+        }
+    });
--- src/main/resources/public/directives/lensForm.js
+++ src/main/resources/public/directives/lensForm.js
@@ -1,14 +1,16 @@
 angular.module("lensAssistant")
-.directive("lensForm", function() {
+.directive("lensForm", function($parse) {
     return {
         link: function(scope, element, attrs) {},
         restrict: "EA",
         templateUrl: "/directives/lensForm.html",
         replace: true,
         scope: {
-            lens: '=lensObject'
+            lens: '=lensObject',
+            formIsValid: '=formIsValid',
+            formName: '@formName'
         },
-        controller: function ($scope, $element, $attrs, $filter) {
+        controller: function ($scope, $element, $attrs, $filter, $parse) {
             $scope.lens = {};
             $scope.datePickerFormat = 'yyyy-MM';
             $scope.openDatePickerPopup = function() {
@@ -17,6 +19,12 @@ angular.module("lensAssistant")
             $scope.datePickerPopup = {
                 opened: false
             };
+            $scope.$watch($scope.formName + '.$valid || ' + $scope.formName + '.$pristine',
+                function(newVal, oldVal){
+                    $scope.formIsValid = newVal || $parse($scope.formName + '.$pristine')($scope);
+                }
+            );
+            // workaround to save validTo as string (not a Date)
             $scope.$watch('lens.validToDate', function(newVal, oldVal){
                 $scope.lens.validTo = $filter('date')(newVal,'yyyy-MM');
             });
